38a9870c9749307f496f01c5d2269894889b4ccd
kallekj <kalle@djervbrant.net>
2018-05-05 19:54:44 +0200

    Work in progress on window

--- src/WindowFX/Window.java
+++ src/WindowFX/Window.java
@@ -2,6 +2,8 @@ package WindowFX;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -9,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
@@ -22,11 +25,9 @@ public class Window extends Application {
 
     private Stage window;
     private Double imageY_Offset;
-    private final int CANVAS_WIDTH = 500, CANVAS_HEIGHT = 500;
-    ImageView imageView;
+    private final int SCENE_WIDTH = 500, SCENE_HEIGHT = 500;
+    ImageView theImageViewer;
     private Scene mainScene;
-    private int initialX;
-    private int initialY;
 
 
     public static void main(String[] args) {
@@ -38,7 +39,7 @@ public class Window extends Application {
 
         window = primaryStage;
         window.setTitle("Main Window");
-        imageView = new ImageView();
+        theImageViewer = new ImageView();
         window.setOnCloseRequest(e ->{
             e.consume();
             window.close();
@@ -65,33 +66,23 @@ public class Window extends Application {
         FileChooser itemSelector = new FileChooser();
         itemSelector.setTitle("Select Image");
 
-        itemOpen.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                File selectedItem = itemSelector.showOpenDialog( primaryStage);
-                if(selectedItem != null){
-                    updateImage(selectedItem);
-                }
+        itemOpen.setOnAction(event -> {
+            File selectedItem = itemSelector.showOpenDialog( primaryStage);
+            if(selectedItem != null){
+                updateImage(selectedItem);
             }
         });
 
         MenuItem itemExit = new MenuItem("Exit", null);
 
         itemExit.setOnAction(e -> Platform.exit());
-        imageView.setPreserveRatio(false);
+        theImageViewer.setPreserveRatio(false);
+        pane.getChildren().add(theImageViewer);
 
         file.getItems().addAll(itemOpen, itemSave, itemExit);
         menuBar.getMenus().add(file);
 
-        // Show Image
-        Image image = new Image("WindowFX\\snyggKille.jpg");
-        ImageView theImageViewer = new ImageView();
-        theImageViewer.setImage(image);
-        theImageViewer.setFitWidth(mainScene.getWidth());
-        theImageViewer.setFitHeight(mainScene.getHeight());
-        pane.getChildren().add(theImageViewer);
 
-       // mainScene
         mainRoot.getChildren().addAll(pane);
 
         window.setScene(mainScene);
@@ -102,10 +93,10 @@ public class Window extends Application {
     public void updateImage(File inputFile){
         String fileURL = inputFile.getAbsolutePath();
         Image newImage = new Image("file:"+fileURL);
-        imageView.setFitWidth(window.getScene().getWidth());
-        imageView.setY(imageY_Offset);
-        imageView.setFitHeight(window.getScene().getHeight() - imageY_Offset);
-        imageView.setImage(newImage);
+        theImageViewer.setFitWidth(window.getScene().getWidth());
+        theImageViewer.setY(imageY_Offset);
+        theImageViewer.setFitHeight(window.getScene().getHeight() - imageY_Offset);
+        theImageViewer.setImage(newImage);
     }
 
 }
