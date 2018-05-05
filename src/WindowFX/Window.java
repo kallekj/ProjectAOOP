ac715d64b9aaafdb387089e45cfad0c1e3a0ccbe
kallekj <kalle@djervbrant.net>
2018-05-06 00:29:52 +0200

    Work in progress on window

--- src/WindowFX/Window.java
+++ src/WindowFX/Window.java
@@ -7,9 +7,13 @@ import javafx.event.EventHandler;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -19,12 +23,13 @@ import javafx.scene.control.MenuBar;
 
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.Map;
 
 
 public class Window extends Application {
 
     private Stage window;
-    private final int SCENE_WIDTH = 700, SCENE_HEIGHT = 700;
+    private int SCENE_WIDTH = 700, SCENE_HEIGHT = 700;
     ImageView theImageViewer;
     private Scene mainScene;
 
@@ -65,7 +70,7 @@ public class Window extends Application {
         FileChooser itemSelector = new FileChooser();
         itemSelector.setTitle("Select Image");
         itemOpen.setOnAction(event -> {
-            File selectedItem = itemSelector.showOpenDialog( primaryStage);
+            File selectedItem = itemSelector.showOpenDialog( window);
             if(selectedItem != null){
                 updateImage(selectedItem);
             }
@@ -77,12 +82,19 @@ public class Window extends Application {
         // This section handles the exit operation
         MenuItem itemExit = new MenuItem("Exit", null);
         itemExit.setOnAction(e -> Platform.exit());
-        theImageViewer.setPreserveRatio(true);
         pane.getChildren().add(theImageViewer);
 
 
+        Menu filter = new Menu("Filter");
+        MenuItem exFilter = new MenuItem("exFilter");
+        Slider slider = new Slider(0, 10, 1);
+        exFilter.setOnAction(event -> SliderBox.display(exFilter.getId(), slider));
+
+
+
         file.getItems().addAll(itemOpen, itemSave, itemExit);
-        menuBar.getMenus().add(file);
+        filter.getItems().add(exFilter);
+        menuBar.getMenus().addAll(file, filter);
         mainRoot.getChildren().addAll(pane);
 
         window.setScene(mainScene);
@@ -92,9 +104,34 @@ public class Window extends Application {
     public void updateImage(File inputFile){
         String fileURL = inputFile.getAbsolutePath();
         Image newImage = new Image("file:"+fileURL);
-        theImageViewer.setFitWidth(mainScene.getWidth());
-        theImageViewer.setFitHeight(mainScene.getHeight());
         theImageViewer.setImage(newImage);
+        if(newImage.getWidth() > 1300){
+            // Calculate the scale change to set correct height value in order to preserve the aspect ratio
+            double imgWidth = newImage.getWidth();
+            double imgScale = 1300/imgWidth;
+            double newImgHeight = newImage.getHeight() * imgScale;
+
+            window.setWidth(1300);
+            window.setHeight(newImgHeight);
+            theImageViewer.fitWidthProperty().bind(mainScene.widthProperty());
+
+        }else{
+            window.setWidth(newImage.getWidth());
+            window.setHeight(newImage.getHeight());
+            theImageViewer.fitWidthProperty().bind(mainScene.widthProperty());
+        }
+
+        theImageViewer.setPreserveRatio(true);
+
+        // Auto resize, needs some tweeks
+//        theImageViewer.setFitWidth(mainScene.getWidth());
+//        theImageViewer.setFitHeight(mainScene.getHeight());
+//        theImageViewer.setImage(newImage);
+//        window.setWidth(newImage.getWidth());
+//        window.setHeight(newImage.getHeight());
+//        theImageViewer.fitWidthProperty().bind(mainScene.widthProperty());
+//        theImageViewer.fitHeightProperty().bind(mainScene.heightProperty());
+//        theImageViewer.setPreserveRatio(true);
     }
 
 }
